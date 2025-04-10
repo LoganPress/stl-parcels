0559f8f9c915a1778e25d46d3a189ce554092e8c
Logan Press <Goldenseek42@gmail.com>
2025-04-09 19:57:12 -0500

    Reduce data size before loading in

--- README.md
+++ README.md
@@ -3,3 +3,6 @@ https://www.stlouis-mo.gov/data/datasets/dataset.cfm?id=82
 
 Method:
 Passed zip folder of shapefile data to mapshaper.org and converted to the WGS84 (EPSG 4326) projection schema. Exported data to GeoJSON. Ran `src/generateSample.js` with desired parameters to produce a sample dataset (not a random sample).
+
+Live deployment status:
+[![Netlify Status](https://api.netlify.com/api/v1/badges/275f4430-67f9-4d4d-9144-3b4a6e2014d0/deploy-status)](https://app.netlify.com/sites/friendly-ptolemy-7d0b59/deploys)
--- src/generateSample.js
+++ src/generateSample.js
@@ -1,40 +1,49 @@
 const fs = require("fs");
-const prcl = require("../data/prcl_geojson/prcl.json");
+const prcl = require(__dirname + "/data/prcl_geojson/prcl.json");
 
-const csv = fs
-  .readFileSync(__dirname + "/../data/prcl_data/PAR2022.csv", "utf8")
-  .split("\n");
+(async () => {
+  const d3 = await import("/path/to/d3-dsv");
+  const csv = fs.readFileSync(
+    __dirname + "/data/prcl_data/PAR2022.csv",
+    "utf8"
+  );
 
-const header = csv.shift();
+  const csvRows = d3.csvParse(csv);
+  const sampleRows = csvRows.filter(
+    (row) => parseFloat(row["LANDAREA"] ?? 0) > 0
+  );
 
-const sampleRows = csv.slice(0);
-// .filter((r) => parseFloat(r.split(",")[18] ?? 0) > 0);
+  console.log(`Sampled ${sampleRows.length} parcels with land area > 0`);
 
-const handles = sampleRows.map((r) => r.split(",")[0]);
+  const handles = sampleRows.map((r) => {
+    // Something spooky is going on with the CSV parsing for the HANDLE column
+    const handleKey = Object.keys(r).find((key) => key.trim() === "HANDLE");
+    return r[handleKey];
+  });
 
-const samplePrcls = prcl.features.filter((f) =>
-  handles.includes(f.properties.HANDLE)
-);
+  // Filter the geojson parcels to only include those in the sample
+  const samplePrcls = prcl.features.filter((f) =>
+    handles.includes(f.properties.HANDLE)
+  );
 
-const sampleParcelJson = {
-  type: prcl.type,
-  features: samplePrcls,
-};
+  const sampleParcelJson = {
+    type: prcl.type,
+    features: samplePrcls,
+  };
 
-sampleRows.unshift(header);
+  const sampleCsv = d3.csvFormat(sampleRows);
 
-const sampleCsv = sampleRows.join("\n");
+  fs.writeFileSync(
+    __dirname + "/data/prcl_data/PAR_SAMPLE.csv",
+    sampleCsv,
+    "utf8"
+  );
 
-fs.writeFileSync(
-  __dirname + "/../data/prcl_data/PAR_SAMPLE.csv",
-  sampleCsv,
-  "utf8"
-);
+  fs.writeFileSync(
+    __dirname + "/data/prcl_geojson/prcl_sample.json",
+    JSON.stringify(sampleParcelJson),
+    "utf8"
+  );
 
-fs.writeFileSync(
-  __dirname + "/../data/prcl_geojson/prcl_sample.json",
-  JSON.stringify(sampleParcelJson),
-  "utf8"
-);
-
-console.log(`Done, created ${handles.length} rows.`);
+  console.log(`Done, created ${handles.length} rows.`);
+})();
